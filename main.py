be1d3dbc252cda208bf844de0de1ea01c802bcc9
mrbtx <mr.teachman@gmail.com>
2021-10-22 19:29:00 -0700

    Base functionality flushed out.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python3
 # =============================================================================
-# Getting to grips with the pandas DataFrame.
+# Author:  Brian Teachman
+# Date:    10/21/2021
 # =============================================================================
+"""Getting to grips with the pandas DataFrame.
+
+    Run this example script using this command:
+
+        python main.py
+
+    Read more here: https://pandas.pydata.org/pandas-docs/stable/reference/frame.html
+"""
+
 from csvdb import DefectDb
 
 # =============================================================================
@@ -19,16 +30,25 @@ if __name__ == '__main__':
     # db.info()
     db.dump()
     # =========================================================================
+    uid = 5
+    # db.delete_row(uid)
     col_name = 'uid'
-    value = 5
-    # db.delete_row(value)
-    db.delete_rows_where(col_name, value)
+    value = uid
+    db.delete_rows_where(value, col_name)  # It never happened
     col_name = 'cause'
     value = "Machine"
-    db.delete_rows_where(col_name, value)
+    db.delete_rows_where(value, col_name)  # Maintenance is on it
     # =========================================================================
-    print(f'\nAfter delete method called on {col_name}={value}: {db.row_count()} rows left.', end='\n\n')
+    print(f'\nAfter delete method called on uid={uid} and {col_name}={value}: {db.row_count()} rows left.', end='\n\n')
     db.dump()
     # =========================================================================
-    print(f'\nEntry #5 and all Machine defects scrubbed. All clean. :)')
+    print(f'\nEntry #5 and all Machine defects scrubbed. All clean. :)', end='\n\n')
     # db.save_db()
+
+    # Hourly reporting
+    print(f"CC count\t| {db.count('CC', 'defect_type')}")
+    print(f"MS count\t| {db.count('MS', 'defect_type')}")
+    print(f"Nuclear Meltdown count\t| {db.count('Nuclear Meltdown', 'defect_type')}")
+    print(f"Machine count\t| {db.count('Machine', 'cause')}")
+    print(f"Operator count\t| {db.count('Operator', 'cause')}")
+    print(f"Gremlins count\t| {db.count('Gremlins', 'cause')}")
--- test_defect_db.py
+++ test_defect_db.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 # =============================================================================
 # Author:  Brian Teachman
-# Date:    10/21/2021
+# Date:    10/22/2021
 # =============================================================================
 """Test cases for the DefectDB module
 
+    Run this test using this command:
+
+        python -m unittest test_defect_db.py
+
+    Read more here: https://docs.python.org/3/library/unittest.html
 """
-# from unittest import TestCase
 import unittest
 import pandas as pd
 from csvdb import DefectDb
 
-# https://docs.python.org/3/library/unittest.html
-
 
 class TestDefectDb(unittest.TestCase):
+
+    # Example csv entry: 99999999999x,A1,CC,Operator,Bussing Station,9999
+    test_defect = {
+        'panel_id': '99999999999x',
+        'location': 'A1',
+        'defect_type': 'CC',
+        'cause': 'Operator',
+        'origin': 'Bussing Station',
+        'uid': 9999
+    }
 
     def setUp(self):
         self.baseline = pd.read_csv('data/datatable.csv')
@@ -23,23 +35,40 @@ class TestDefectDb(unittest.TestCase):
 
     def test_row_count(self):
         self.assertEqual(self.db.row_count(), self.data_size, 'incorrect number of rows')
-        self.assertEqual(self.db.row_count(), 13, 'incorrect number of rows')
+
+    def test_contains(self):
+        self.assertTrue(self.db.contains(5), 'UID #5 definitely exists')
+        self.assertTrue(self.db.contains(15), 'UID #15 definitely exists')
+        self.assertFalse(self.db.contains(20), 'Our dataset has 17 entries, not 20')
+        self.assertTrue(self.db.contains('CC', 'defect_type'), 'Cells cracks are pretty common')
+        self.assertFalse(self.db.contains('Nuclear Meltdown', 'defect_type'), 'No nukes here')
+        self.assertFalse(self.db.contains('Monkey', 'cause'), 'A monkey?')
+        self.assertTrue(self.db.contains('Machine', 'cause'), 'A monkey?')
+
+    def test_count(self):
+        self.assertEqual(self.db.count('MS', 'defect_type'), 6, 'There are actually 6 missed solders in the dataset')
+        self.assertEqual(self.db.count('Nuclear Meltdown', 'defect_type'), 0, '0 nukes here')
+
+    def test_insert_row(self):
+        self.db.insert_row(self.test_defect)
+        self.assertEqual(self.db.row_count(), self.data_size + 1, 'Does not have added row')
+        self.assertTrue(self.db.contains(self.test_defect['uid']), 'No rows contain added UID')
 
     def test_delete_row(self):
         self.db.delete_row(5)
-        self.assertEqual(self.db.row_count(), self.data_size - 1, 'incorrect number of rows')
-        self.assertFalse(5 in self.db._data.uid)
-
-    def test_delete_row_where_uid_doesnt_exist(self):
-        self.db.delete_row(15)
-        self.assertEqual(self.db.row_count(), 13, 'incorrect number of rows')
+        self.assertEqual(self.db.row_count(), self.data_size - 1, 'Incorrect number of rows')
+        self.assertFalse(self.db.contains(5), 'UID #5 should not exist here since we deleted it')
+        # self.assertFalse(5 in self.db._data.uid)
 
     def test_delete_rows_where(self):
-        self.fail()
+        self.db.delete_rows_where('MS', 'defect_type')  # There are 6 of these in our dataset
+        self.assertEqual(self.db.row_count(), self.data_size - 6, 'Incorrect number of rows')
 
-    def test_insert_row(self):
-        self.fail()
+    def test_delete_row_where_uid_doesnt_exist(self):
+        self.db.delete_row(self.data_size + 1)  # Add one more than exist in the set
+        self.assertEqual(self.db.row_count(), self.data_size, 'Nothing deleted, should have same number of rows')
 
+    @unittest.skip
     def test_save_db(self):
         self.fail()
 
